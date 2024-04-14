51176cd1535ae13c13e29ea30e9be9ee7106e4a1
nanahoshi95 <yosuke09058@gmail.com>
2024-04-14 15:49:17 +0900

    add: エネルギーカード関連のモデル作成

---
+++ PCG/PCG/Models/DataBase/EnergyCardData.swift
@@ -0,0 +1,8 @@
+//
+//  EnergyCardData.swift
+//  PCG
+//
+//  Created by yamaji on 2024/04/14.
+//
+
+import Foundation
---
+++ PCG/PCG/Models/Types/EnergyCard.swift
@@ -0,0 +1,8 @@
+//
+//  EnergyCard.swift
+//  PCG
+//
+//  Created by yamaji on 2024/04/14.
+//
+
+import Foundation
