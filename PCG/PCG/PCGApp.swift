15998c0d39356e0efbd36424cf772693df019a1c
nanahoshi95 <yosuke09058@gmail.com>
2024-04-14 12:36:54 +0900

    Initial commit

---
+++ PCG/PCG/PCGApp.swift
@@ -0,0 +1,17 @@
+//
+//  PCGApp.swift
+//  PCG
+//
+//  Created by yamaji on 2024/04/14.
+//
+
+import SwiftUI
+
+@main
+struct PCGApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
